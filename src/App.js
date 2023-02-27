37ede4ac4a2413bf8d6baea2bfa85b22ea6488b1
shubham78 <sharaff0shubham@gmail.com>
2023-02-27 19:34:42 +0530

    Login Form Completed with api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Register from "./Register";
+import axios from "axios";
 function App() {
+  axios.defaults.baseURL ='http://localhost:4000' 
+  // witCredentials we are using here so we can set cookies from api 
+  axios.defaults.withCredentials = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Register />
+  
   );
 }
 
